01b7b3579f33f859ab549f259b37c529247cbbbc
Paul Lockett <plockett@flexport.com>
2024-02-07 16:20:46 -0800

    Update svelte.config.js for port

--- svelte.config.js
+++ svelte.config.js
@@ -18,6 +18,8 @@ const config = {
 
 		paths: {
 			base: process.env.APP_BASE || "",
+			port: process.env.Port || 3000,
+			host: process.env.Host || 0.0.0.0,
 		},
 		csrf: {
 			// handled in hooks.server.ts, because we can have multiple valid origins
